5bf1d46b51636e383819ff3a54a31dac1a86715a
root <root@Symfony>
2015-07-01 11:23:33 +0200

    Modification .gitignore

--- src/Gogole/CalendarBundle/Controller/CalendarController.php
+++ src/Gogole/CalendarBundle/Controller/CalendarController.php
@@ -4,10 +4,40 @@ namespace Gogole\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+use Gogole\CalendarBundle\Entity\Event;
+use Symfony\Component\HttpFoundation\Request;
+
+use Gogole\UserBundle\Entity\User;
+
+class CalendarController extends Controller
 {
-    public function indexAction($name)
+
+    public function calendarAction( Request $request )
     {
-        return $this->render('GogoleCalendarBundle:Calendar:calendar.html.twig', array('name' => $name));
+    	$em = $this->getDoctrine()->getManager();
+    	$repoCalendar = $em->getRepository('GogoleCalendarBundle:Event');
+    	$allEvent = $repoCalendar->findAll();
+
+    	$event = new Event();
+    	$formBuilder = $this->get('form.factory')->createBuilder('form', $event);
+    	$formBuilder
+    		->add('title')
+    		->add('description')
+    		->add('start')
+    		->add('end')
+    		->add('Envoyer', 'submit', array('attr' => array('class' => 'btn btn-default')));
+    	$form = $formBuilder->getForm();
+    	$form->handleRequest($request);
+
+    	if ($form->isValid())
+    	{
+    		$em->persist($event);
+    		$em->flush();
+    	}
+
+    	return $this->render('GogoleCalendarBundle:Calendar:calendar.html.twig', array(
+    		'form'     => $form->createView(),
+    		'allEvent' => $allEvent,
+    	));
     }
 }
